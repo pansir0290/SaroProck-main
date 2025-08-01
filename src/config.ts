782a2d7c449dfadcac139b7cbf093bf62422bf28
pansir0290 <pansir029@gmail.com>
2025-08-01 15:07:47 +0800

    配置评论系统

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // src/config.ts
 // 针对博主特定的数据
 export const ADMIN_USER = {
-  nickname: "EveSunMaple",
-  email: "evesunmaple@outlook.com",
-  website: "https://www.saroprock.com",
-  avatar: "https://www.saroprock.com/avatar.webp",
+  nickname: "pansir029",
+  email: "pansir029@gmail.com",
+  website: "https://www.34310889.xyz",
+  avatar: "https://www.34310889.xyz/avatar.webp",
 };
 
 // 你不希望用户使用的名称与邮箱
-export const SENSITIVE_USERS = ["evesunmaple", "EveSunMaple", "sunmaple", "SunMaple", "admin", "博主", "evesunmaple@outlook.com"];
+export const SENSITIVE_USERS = ["pansir029", "EveSunMaple", "sunmaple", "SunMaple", "admin", "博主", "pansir029@gmail.com"];
